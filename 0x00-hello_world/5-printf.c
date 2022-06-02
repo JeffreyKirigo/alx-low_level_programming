902a1d5086f8fd128ee7d96430ac50b132a6da13
JeffreyKirigo <jeffreyem99@gmail.com>
2022-06-02 20:17:46 +0000

    Using the printf statement

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+/*
+ * This program prints an output using
+ * the printf()
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
