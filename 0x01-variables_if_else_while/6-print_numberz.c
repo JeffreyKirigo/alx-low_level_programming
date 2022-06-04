cba1ce2b0976801c88a7686fde36ce201bd8120b
JeffreyKirigo <jeffreyem99@gmail.com>
2022-06-04 01:23:59 +0000

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+/* more headers go there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int number;
+
+	for (number = 48; number <= 57; number++)
+	{
+		putchar(number);
+	}
+	putchar('\n');
+	return (0);
+
+}
