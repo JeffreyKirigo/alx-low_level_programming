71884d1ba812321201af984a00dc460ff88923a2
JeffreyKirigo <jeffreyem99@gmail.com>
2022-06-08 01:05:58 +0000

    Prints the sign of a number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ *
+ * @n: number to be computed for absolute value
+ *
+ * Return: 0 Success
+ */
+int _abs(int n)
+{
+	return(((n >= 0) ? n: n* -1));
+}
