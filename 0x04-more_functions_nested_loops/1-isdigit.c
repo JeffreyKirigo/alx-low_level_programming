42e2af960e6bf63c47da7c10ccc9b76fce6f69f2
JeffreyKirigo <jeffreyem99@gmail.com>
2022-06-09 23:46:36 +0000

    Checks if parameter passed is a digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks if c is a digit
+ * @c: to be checked
+ * Return: 0 if c is a digit else 1
+ */
+int _isdigit(int c)
+{
+	return ((c >= 48 && c <= 57) ? 0 : 1);
+}
