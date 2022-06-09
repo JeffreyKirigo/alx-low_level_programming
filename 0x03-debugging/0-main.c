13498bddbb9192d033d021b8cae5dd93279d9b3e
JeffreyKirigo <jeffreyem99@gmail.com>
2022-06-09 15:31:14 +0000

    Debugging on C

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - tests function the checks if integer is +ve pr -Ve
+ *
+ * Return: 0 Success
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 98;
+	positive_or_negative(i);
+
+	return (0);
+}
