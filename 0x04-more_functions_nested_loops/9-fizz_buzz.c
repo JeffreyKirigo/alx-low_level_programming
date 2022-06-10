2850812cc3de7277f5bb15b2fc2749508b87b655
JeffreyKirigo <jeffreyem99@gmail.com>
2022-06-10 14:06:09 +0000

    Fizz Buzz Test

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,7 +11,7 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0 && i % 5 == 0)
+		if (i % 15 == 0)
 			printf("FizzBuzz ");
 		else if (i % 3 == 0)
 			printf("Fizz ");
@@ -20,6 +20,7 @@ int main(void)
 		else
 			printf("%d ", i);
 	}
+	printf("\n");
 
 	return (0);
 }
