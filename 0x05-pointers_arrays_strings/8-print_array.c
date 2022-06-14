20990e44e5c5488f4235922fcff2c17db222c63d
JeffreyKirigo <jeffreyem99@gmail.com>
2022-06-14 02:59:10 +0000

    Arrays not pointers

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_array - prints elements of an array
+ * @a: array
+ * @b: number of elements
+ *
+ * Return: void
+ */
+void print_array(int *a, int b)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", *(a + i));
+		if (i != (n - 1))
+			printf(", ");
+	}
+	printf("\n");
+}
