5fac09514a0316eb22bc4ea037972f7eeec8a3e0
JeffreyKirigo <jeffreyem99@gmail.com>
2022-07-05 01:29:05 +0000

    Poppy -structures declaration

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,14 @@
+/**
+ * struct dog - declaration of struct dog
+ * @name: Name of dog
+ * @age: Age of dog
+ * @owner: Owner's name
+ *
+ * Description: declaration of struct dog and porperties
+ */
 struct dog
 {
 	char *name;
-	float *age;
+	float age;
 	char *owner;
-}
+};
