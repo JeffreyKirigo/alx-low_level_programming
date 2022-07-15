171c3274d6962029a21ffde416601e585e68f4aa
JeffreyKirigo <jeffreyem99@gmail.com>
2022-07-15 02:56:04 +0000

    Executes before main

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -4,8 +4,9 @@
  * b4main - executes before main
  * Return: void
  */
-void b4main() __attribute__((constructor));
-void b4main()
+void b4main(void) __attribute__((constructor));
+void b4main(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,
+			\nI bore my house upon my back!\n");
 }
